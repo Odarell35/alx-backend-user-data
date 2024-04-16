87d325b8a67efa15cb01de587ec8c4f9acd28f82
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-16 19:50:20 +0200

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,3 +19,15 @@ class BasicAuth(Auth):
         hd = authorization_header.split(' ')
 
         return hd[1] if hd[0] == 'Basic' else None
+
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
+        if not decoded_base64_authorization_header or\
+           not isinstance(decoded_base64_authorization_header, str)\
+           or ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        extract = decoded_base64_authorization_header.split(':', 1)
+        return (extract[0], extract[1]) if extract else (None, None)
