2df511628a397d11df93ce1d86d89b096c521714
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-17 08:23:17 +0200

    indentation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,7 +67,7 @@ class BasicAuth(Auth):
         return None
 
 
-def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User'):
         """ overloads Auth and
             retrieves the User instance for a request
         """
