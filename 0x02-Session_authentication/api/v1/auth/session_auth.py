45f22c845b58431558e74698fa6f98dc76384f2d
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-04-19 09:26:31 +0200

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,5 +7,12 @@ import uuid
 
 
 class SessionAuth(Auth):
-  """ a class SessionAuth that inherits from Auth"""
-  pass
+    """ a class SessionAuth that inherits from Auth"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a Session ID for a user_id """
+        if isinstance(user_id, str):
+            session_id = str(uuid.uuid4())
+            SessionAuth.user_id_by_session_id[session_id] = user_id
+            return session_id
