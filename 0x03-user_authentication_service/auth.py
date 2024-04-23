c23843475d48ab88519f1e91ccad51ec2398a666
Odarell35 <leratomawash0@gmail.com>
2024-04-23 09:05:12 +0000

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,8 +9,30 @@ from bcrypt import hashpw, gensalt, checkpw
 from typing import TypeVar
 from sqlalchemy.orm.exc import NoResultFound
 
+
 def _hash_password(password: str) -> str:
-        """Generate a salted hash of the input password"""
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
+    """Generate a salted hash of the input password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+
+    def register_user(self,email: str, password: str) -> User:
+        """ method to register user
+            attr: email, password
+            return user object itf avail
+        """
+        try:
+            avail_user = self._db.find_user_by(email=email)
+            if avail_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+           return self._db.add_user(email, _hash_password(password))
