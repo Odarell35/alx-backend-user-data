809edc685e827769cea200a2325f4825f8cd03b2
Odarell35 <leratomawash0@gmail.com>
2024-04-24 07:55:53 +0000

    11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -22,6 +22,19 @@ def users() -> str:
     except Exception:
         return jsonify({"messege": "email already registered"}), 400
 
+@app.route('/session', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+    if not valid_login:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
+
 
 
 if __name__ == "__main__":
