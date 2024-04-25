34d7346f6c8b3b527906482455bb3c45010fae41
Odarell35 <leratomawash0@gmail.com>
2024-04-25 12:10:51 +0000

    updated task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 class User(Base):
     """defining the class model"""
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key= True)
     email = Column(String(250), nullable= False)
     hashed_password = Column(String(250), nullable= False)
